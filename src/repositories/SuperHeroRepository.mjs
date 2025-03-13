cb0fd02ae853ef666d6d787d4df60b1801146689
Jeremias <jerepizzaps3@gmail.com>
2025-03-13 00:20:11 -0300

    Servidor funcionando

---
+++ src/repositories/SuperHeroRepository.mjs
@@ -0,0 +1,26 @@
+import superHero from '../models/SuperHero.mjs';
+import IRepository from './Irepository.mjs';
+
+class SuperHeroRepository extends IRepository{
+    async obtenerPorId(id){
+        return await superHero.findById(id);
+    }
+
+    async obtenerTodos(){
+        return await superHero.find({});
+    }
+
+    async obtenerMayoresDe30() {
+        return await superHero.find({ edad: { $gt: 30 } });
+    }
+
+    /*async obtenerMenoresDe30() {
+        return await SuperHero.find({ edad: { $lt: 30 } });
+    }*/
+
+    async buscarPorAtributo(atributo, valor) {
+        return await superHero.find({ [atributo]: valor });
+    }
+}
+
+export default new SuperHeroRepository();
